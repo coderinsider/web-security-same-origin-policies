e383b102950d5789c083166676a6abec4dd848bf
Myat Ko Ko <coderinsider1@gmail.com>
2021-10-11 22:05:44 +0630

    Implement the strick transport security header

---
+++ 4-1-implement-the-strick-transport-security-header/site/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+require('dotenv').config()
+
+app.set('port', (process.env.PORT || 5000));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public');
+
+app.get('/', function (request, response) {
+  var env = process.env.APP_ENV;
+  if (env == 'staging') {
+    var envName = 'staging'
+  } else if (env == 'production') {
+    var envName = 'production'
+  } else {
+    var envName = 'review app'
+  }
+  response.set({
+    "Content-Security-Policy": "default-src 'none'; script-src 'self' https://[[RESOURCES]] https://ajax.googleapis.com https://platform.linkedin.com https://www.linkedin.com; frame-src https://www.linkedin.com; img-src https://[[RESOURCES]]; style-src https://[[RESOURCES]] 'unsafe-inline'; font-src https://[[RESOURCES]]"
+  });
+  response.set({
+    "Strict-Transport-Security": "max-age=31536000"
+  })
+  response.render('index.html', { env: envName });
+});
+
+app.listen(app.get('port'), function () {
+  console.log("Node app running at localhost:" + app.get('port'));
+});
+
+module.exports = app
