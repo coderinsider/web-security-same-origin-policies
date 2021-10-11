074fc52c1f0f5290c5332ae83e25b27c9f2b289f
Myat Ko Ko <coderinsider1@gmail.com>
2021-10-11 19:59:56 +0630

    Building a content security policies header

---
+++ 3-1-build-a-content-security-policies-header/resources/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+require('dotenv').config()
+
+app.set('port', (process.env.PORT || 5000));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public');
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "https://[[SITE]]");
+  res.setHeader("Vary", "Origin");
+  return next();
+});
+app.use(express.static('files'));
+
+app.listen(app.get('port'), function () {
+  console.log("Node app running at localhost:" + app.get('port'));
+});
+
+module.exports = app
